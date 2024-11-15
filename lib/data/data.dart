9d8078d3bfc3479e87aaf5cf6f5fa6aa61c8d910
SatimovDevoloper <dilshodsatimov990@mail.ru>
2024-11-15 12:08:23 +0500

    homeScreen tayyor bo'ldi

---
+++ lib/data/data.dart
@@ -0,0 +1,121 @@
+class Data {
+
+
+  static List dataList = [
+    {
+      "name": "Orol dengizi",
+      "image": "assets/images/img_orol.png",
+      "location": "Qoraqalog‘iston",
+      "category": "Suv",
+      "description":
+          """Orol dengizi – Oʻrta Osiyodagi eng katta berk koʻl. Maʼmuriy jihatdan Orol dengizining 
+          yarmidan koʻproq janubi-gʻarbiy qismi Oʻzbekiston, shimoli-sharqiy qismi Qozogʻiston hududida 
+          joylashgan. Oʻtgan asrning 60-yillarigacha Orol dengizi maydoni orollari bilan oʻrtacha 
+          68,0 ming km² ni tashkil etgan. Kattaligi jihatidan dunyoda toʻrtinchi (Kaspiy dengizi,
+           Amerikadagi Yuqori koʻl va Afrikadagi Viktoriya koʻlidan keyin), Yevrosiyo materigida 
+           (Kaspiydan keyin) ikkinchi oʻrinda edi. Dengiz shimoli-sharqdan janubi-gʻarbga choʻzilgan,
+            uz. 428 km, eng keng joyi 235 km (45° shahrik.) boʻlgan. Havzasining maydoni 690 ming km²,
+             suvining hajmi 1000 km³, oʻrtacha chuq. 16,5 m atrofida oʻzgarib turgan. """,
+      "score": "4.7",
+      "isFamous": true
+    },
+    {
+      "name": "Zomin",
+      "image": "assets/images/img_zomin.png",
+      "location": "Jizzax",
+      "category": "Sanatoriya",
+      "description":
+          """Zomin milliy bog‘ida, dengiz sathidan 2 ming metr balandlikda "Zomin" davolash-sog‘lomlashtirish
+       sanatoriysi joylashgan.O‘ta toza havo, tog‘ manzaralari, ignabargli o‘simliklar - bularning barchasi
+        kattalar va bolalarga sog‘lom turmush tarzi uchun eng yaxshi sharoitlarni yaratadi.Sanatoriyda zamonaviy 
+        tibbiyot uskunalari mavjud, shuningdek diagnostika, EKG, ultratovush va stomatologiya bo‘limlari ishlab 
+        turibdi. Tashxisdan keyin mehmonlarga zarur terapiya va tegishli davolash protseduralari tayinlanadi.""",
+      "score": "5",
+      "isFamous": false
+    },
+    {
+      "name": "Zomin tog‘lari",
+      "image": "assets/images/img_zomin_toglari.png",
+      "location": "Jizzax",
+      "category": "Tog‘",
+      "description":
+          """Hudud ikki qismdan tarkib topgan: Turkiston tog`larining shimoliy tizmalarida joylashgan, bir-biri bilan chegaradosh Zomin Davlat qo`riqxonasi va Zomin Xalq bog`i. Zomin Davlat qo`riqxonasi Zomin va Baxmal tumanlari hududida, 26840 ga maydonga ega bo`lib, 1960 yil tashkil qilingan. Zomin Xalq bog`i 1978 yil tashkil qilingan, Jizzax viloyatining Zomin tumanida 24110 ga maydonni tashkil qiladi. Qo`riqxona o`z hududidagi barcha tabiiy resurslarni muhofaza qilish va daxlsizligini ta’minlaydi. Xalq Bog`i noyob tog` ekotizimlarni saqlash, tiklash va rekreatsion tadqiq qilish maqsadida tashkil etilgan. Qarag`ay o`rmonlari hududdagi eng yirik o`rmonlardan hisoblanadi. O`zbekiston Qizil kitobiga kiritilgan noyob hayvonot turlari (14 ta) va o`simliklari (18 ta) mavjud. Hudud 1913 yildan buyon turli darajada muhofaza ostiga olingani sabab, noyob tabiiy resurslar saqlanib qolgan.""",
+      "score": "4.5",
+      "isFamous": true
+    },
+    {
+      "name": "Xiva shahri",
+      "image": "assets/images/img_xiva.png",
+      "location": "Xorazm",
+      "category": "Shahar",
+      "description":
+          """Xiva – Oʻzbekiston Respublikasining Xorazm viloyatidagi shahar. Xiva tumani markazi. Oʻzbekistonning shimoliy gʻarbida, Xorazm viloyatning janubida, Amudaryoning chap sohilida, daryodan 40 km janubida, 95 m balandlikda joylashgan. Shahar yonidan Polvonyop (qadimiy Xeykaniq) kanali oʻtgan. Yaqin temir yoʻl stansiyasi – Urganch (30 km). Maydoni 0,08 ming km². 2022-yil 1-yanvar holatiga Xiva tuman doimiy aholisining soni 149 698 kishi, Xiva shahri – 95 246 kishini tashkil qilgan. Shaharning qadimgi qismidagi juda koʻp arxitektura yodgorliklariga boy boʻlgan Ichan-Qal’a Sharqning ekzotik shahar timsolini oʻzida saqlab qolgan afsonaviy shahardir.""",
+      "score": "5",
+      "isFamous": true
+    },
+    {
+      "name": "Samarqand shahri",
+      "image": "assets/images/img_samarqand.png",
+      "location": "Samarqand",
+      "category": "Shahar",
+      "description":
+          """Samarqand – Oʻzbekiston Respublikasi Samarqand viloyatidagi qadimiy shahar. Amir Temur ordeni sohibi. Viloyatning maʼmuriy, iqtisodiy va madaniy markazi (1938-yildan). 1925-1930-yillarda Respublika poytaxti. Oʻzbekistonning janubi-gʻarbida, Zarafshon vodiysining markaziy qismida (Dargʻom va Siyob kanallari orasida) joylashgan. Oʻrtacha 695 m balandlikda. Toshkentdan 300 km. Samarqanddan Toshkent—Dushanbe, Toshkent—Turkmanboshi, Toshkent—Uchquduq—Qoʻngʻirot temir yoʻllari, Katta Oʻzbek trakti (Toshkent—Termiz yoʻli) oʻtadi. Shahar aholisi va xoʻjaliklari Shovdor, Bogʻishamol ariqlaridan suv oladi. Iyulning oʻrtacha harorati 25,9°, eng baland harorat 40—42°, yanvar oʻrtacha harorati 0,2°, eng past harorat —26°. Maydoni −120 km². Aholisi-3 mln 50 ming kishi (2024). 2022-yilning 15-16-sentyabr kunlari ShHTga aʼzo davlat rahbarlarining tartib boʻyicha 22-yigʻilish boʻlgan Samarqand sammiti boʻlib oʻtgan.""",
+      "score": "5",
+      "isFamous": true
+    },
+    {
+      "name": "Buxoro shahri",
+      "image": "assets/images/img_buxoro.png",
+      "location": "Buxoro",
+      "category": "Shahar",
+      "description":
+          """Buxoro — Oʻzbekiston Respublikasining qadimiy shaharlaridan biri, Buxoro viloyatining maʼmuriy, iqtisodiy va madaniy markazi. Oʻzbekistonning ilk poytaxti. Buyuk ipаk yoʻlida yirik tijorat markazlaridan boʻlgan. Oʻzbekistonning janubiy-gʻarbida, Zarafshon daryosi quyi oqimida joylashgan. Toshkentdan 616 km. Buxoro 2 ta shahar rayoni (Fayzulla Xoʻjayev va Toʻqimachilik)ga boʻlingan. Aholisi 290 000 (2019). Aholisining katta qismini oʻzbeklar tashkil etadi. Oliy ta‘lim muassasalari: Buxoro davlat universiteti, Buxoro davlat pedagogika instituti, Buxoro muhandislik-texnologiya instituti.""",
+      "score": "5",
+      "isFamous": true
+    },
+    {
+      "name": "Humson sanatoriyasi",
+      "image": "assets/images/img_humson.png",
+      "location": "Toshkent viloyati",
+      "category": "Sanatoriya",
+      "description":
+          """Humson Buloq sanatoriy-kurort majmuasi yurak-qon tomir tizimi, nafas olish organlari va tayanch-harakat apparati kasalliklariga ega boʻlgan odamlarni profilaktik davolashga ixtisoslashgan. Majmua fizioterapevtik va gidroterapevtik muolajalarini oʻtkazish uchun yuqori samarali GBO va RMS uskunalari bilan jihozlangan.Humson Buloq umumiy hisobda 155 nafar mehmon uchun moʻljallangan keng terrasa va ayvonga ega ikki qavatli toʻrtta alohida turuvchi kottejlar va turli toifadagi shinam xonalarga ega toʻrtta qulay korpuslarni taklif qiladi. Sanatoriy mehmonlari uchun kompleks ovqatlanish 130 kishili restoranda oʻtkaziladi, shuningdek, bu yerda 8 kishi uchun uchta alohida VIP xonalar mavjud.""",
+      "score": "4.2",
+      "isFamous": false
+    },
+    {
+      "name": "Charvoq suv ombori",
+      "image": "assets/images/img_charvoq.png",
+      "location": "Toshkent viloyati",
+      "category": "Suv",
+      "description":
+      """Chorvoq suv ombori — Chirchiq daryosining boshlanish qismida qurilgan (1963—70) gidrotexnika inshooti. Q.x.ni bir meʼyorda suv bilan taʼminlash, toshqinlarning oldini olish va gidrotexnika maqsadlarida barpo etilgan. Daryo suvini mavsumiy tartibga soladi. Suv ombori Chatqol va Ugom toglari orasiga qurilgan. Tosh tuproqli toʻgʻoni uz. 768 m, balandligi 168 m, yuqori qismining eni 12 m. Toʻgʻonda Chorvoq GESni suv bilan taʼminlash uchun uz. 800 m va diametri 11 m boʻlgan 2 ta tunnel bor. Ch.s.o.ning toʻla sigʻimi 2006 mln. m3, shundan foydali suv hajmi 1580 mln. m3, suv yuzasi sathi 40,1 km², eni ayrim joylarda 10 km, uz. 19 km, maksimal chuq. 131 m, oʻrtacha chuq. 55 m. Suv omboriga Piskom, Koʻksuv, Chatqol daryolari quyiladi.""",
+      "score": "4",
+      "isFamous": false
+    },
+    {
+      "name": "Oqtosh tog‘lari",
+      "image": "assets/images/img_oqtosh.png",
+      "location": "Toshkent viloyati",
+      "category": "Tog‘",
+      "description":
+      """Chorvoq suv ombori — Chirchiq daryosining boshlanish qismida qurilgan (1963—70) gidrotexnika inshooti. Q.x.ni bir meʼyorda suv bilan taʼminlash, toshqinlarning oldini olish va gidrotexnika maqsadlarida barpo etilgan. Daryo suvini mavsumiy tartibga soladi. Suv ombori Chatqol va Ugom toglari orasiga qurilgan. Tosh tuproqli toʻgʻoni uz. 768 m, balandligi 168 m, yuqori qismining eni 12 m. Toʻgʻonda Chorvoq GESni suv bilan taʼminlash uchun uz. 800 m va diametri 11 m boʻlgan 2 ta tunnel bor. Ch.s.o.ning toʻla sigʻimi 2006 mln. m3, shundan foydali suv hajmi 1580 mln. m3, suv yuzasi sathi 40,1 km², eni ayrim joylarda 10 km, uz. 19 km, maksimal chuq. 131 m, oʻrtacha chuq. 55 m. Suv omboriga Piskom, Koʻksuv, Chatqol daryolari quyiladi.""",
+      "score": "4.3",
+      "isFamous": false
+    }
+  ];
+
+  static List<String> getCategories() {
+    List<String> categories = ["Barchasi"];
+    categories.addAll(dataList.map((item) => item["category"] as String).toList());
+    return categories.toSet().toList();
+  }
+
+  static List getFamousItems(){
+    return dataList.where((item) => item["isFamous"] == true).toList();
+  }
+
+  static List getCategoryList(String category){
+    return category == "Barchasi" ? dataList : dataList.where((item)=>item["category"]==category).toList();
+  }
+}
--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:home_work_4/config/colors.dart';
 import 'package:home_work_4/config/strings.dart';
+import 'package:home_work_4/data/data.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -11,6 +12,13 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   final searchController = TextEditingController();
+  int selectedIndex = 0;
+
+  void onClickCategory(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +27,7 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         backgroundColor: AppColor.backgroundColor,
         title: Padding(
-          padding: const EdgeInsets.only(left: 16.0),
+          padding: const EdgeInsets.only(left: 8.0),
           child: Text(
             AppText.homeText1,
             style: const TextStyle(
@@ -31,30 +39,276 @@ class _HomeScreenState extends State<HomeScreen> {
         crossAxisAlignment: CrossAxisAlignment.start,
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
-        const SizedBox(
-        height: 16,
-      ),
-      Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 32,
-        ),
-        child: TextField(
-            controller: searchController,
-            decoration: const InputDecoration(
-                suffixIcon:  Icon(Icons.search, color: Color(0xffd6d2d2),),
-                border: OutlineInputBorder(borderSide: BorderSide.none),
-            filled: true,
-            fillColor: Color(0xff263238),
-            hintText: "Qidiruv",
-            hintStyle: TextStyle(
-                color: Color(0xffa5a5a5),
-                fontSize: 16,
-                fontWeight: FontWeight.w300)),
+          const SizedBox(
+            height: 16,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 24,
+            ),
+            child: TextField(
+              controller: searchController,
+              decoration: const InputDecoration(
+                  suffixIcon: Icon(
+                    Icons.search,
+                    color: Color(0xffd6d2d2),
+                  ),
+                  border: OutlineInputBorder(borderSide: BorderSide.none),
+                  filled: true,
+                  fillColor: Color(0xff263238),
+                  hintText: "Qidiruv",
+                  hintStyle: TextStyle(
+                      color: Color(0xffa5a5a5),
+                      fontSize: 16,
+                      fontWeight: FontWeight.w300)),
+            ),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          SizedBox(
+            height: 34,
+            child: ListView.separated(
+                padding: const EdgeInsets.symmetric(horizontal: 24),
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  return GestureDetector(
+                    onTap: () {
+                      onClickCategory(index);
+                    },
+                    child: Container(
+                      alignment: Alignment.center,
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 16, vertical: 8),
+                      decoration: BoxDecoration(
+                        color: selectedIndex == index
+                            ? const Color(0xff5edfff)
+                            : const Color(0xff263238),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: Text(
+                        Data.getCategories()[index],
+                        style: TextStyle(
+                            color: selectedIndex == index
+                                ? const Color(0xff263238)
+                                : Colors.white,
+                            fontSize: 12,
+                            fontWeight: FontWeight.w500),
+                      ),
+                    ),
+                  );
+                },
+                separatorBuilder: (context, index) {
+                  return const SizedBox(
+                    width: 8,
+                  );
+                },
+                itemCount: Data.getCategories().length),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          Expanded(
+            child: ListView(
+              children: [
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 32),
+                  child: Text(
+                    "Mashhurlar",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600),
+                  ),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                SizedBox(
+                  height: 180,
+                  child: ListView.separated(
+                      padding: const EdgeInsets.symmetric(horizontal: 24),
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (context, index) {
+                        return Card(
+                          shape: RoundedRectangleBorder(
+                              side: BorderSide.none,
+                              borderRadius: BorderRadius.circular(8)),
+                          child: Container(
+                            width: 120,
+                            height: 180,
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                  image: AssetImage(
+                                      Data.getFamousItems()[index]["image"]),
+                                  fit: BoxFit.cover),
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            child: Stack(
+                              alignment: Alignment.bottomLeft,
+                              children: [
+                                Container(
+                                  width: double.infinity,
+                                  height: double.infinity,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(8),
+                                    gradient: LinearGradient(
+                                        colors: [
+                                          Colors.black.withOpacity(.7),
+                                          Colors.transparent
+                                        ],
+                                        begin: Alignment.bottomCenter,
+                                        end: Alignment.topCenter,
+                                        stops: [0.0, 0.4]),
+                                  ),
+                                ),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    Padding(
+                                        padding: const EdgeInsets.symmetric(
+                                            horizontal: 8),
+                                        child: Text(
+                                          Data.getFamousItems()[index]["name"],
+                                          style: const TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 12,
+                                              fontWeight: FontWeight.w500),
+                                        )),
+                                    Padding(
+                                        padding: const EdgeInsets.only(
+                                            left: 8, bottom: 8),
+                                        child: Text(
+                                          Data.getFamousItems()[index]
+                                              ["location"],
+                                          style: const TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 10,
+                                              fontWeight: FontWeight.w300),
+                                        ))
+                                  ],
+                                )
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                      separatorBuilder: (context, index) {
+                        return const SizedBox(
+                          width: 12,
+                        );
+                      },
+                      itemCount: Data.getFamousItems().length),
+                ),
+                const SizedBox(
+                  height: 32,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 24),
+                  child: Text(
+                    Data.getCategories()[selectedIndex],
+                    style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500),
+                  ),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                ListView.separated(
+                    padding: const EdgeInsets.symmetric(horizontal: 24),
+                    physics: const NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        width: double.infinity,
+                        height: 200,
+                        decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage(
+                                Data.getCategoryList(
+                                        Data.getCategories()[selectedIndex])[
+                                    index]["image"],
+                              ),
+                              fit: BoxFit.cover,
+                            ),
+                            borderRadius: BorderRadius.circular(16)),
+                        child: Stack(
+                          alignment: Alignment.bottomLeft,
+                          children: [
+                            Container(
+                              width: double.infinity,
+                              height: double.infinity,
+                              decoration: BoxDecoration(
+                                gradient: LinearGradient(
+                                    colors: [Colors.black, Colors.transparent],
+                                    begin: Alignment.bottomCenter,
+                                    end: Alignment.topCenter,
+                                    stops: [0.0, 0.3]),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+                              child: Row(
+                                children: [
+                                  Column(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      Text(
+                                        Data.getCategoryList(Data.getCategories()[
+                                            selectedIndex])[index]["name"],
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 12,
+                                            fontWeight: FontWeight.w500),
+                                      ),
+                                      Text(
+                                        Data.getCategoryList(Data.getCategories()[
+                                        selectedIndex])[index]["location"],
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 10,
+                                            fontWeight: FontWeight.w300),
+                                      )
+                                    ],
+                                  ),
+                                  Spacer(),
+                                  Text(
+                                    Data.getCategoryList(Data.getCategories()[
+                                    selectedIndex])[index]["score"],
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.w500),
+                                  ),
+                                  SizedBox(width: 8,),
+                                  Icon(Icons.star, color: Colors.amber,size: 24,)
 
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                      );
+                    },
+                    separatorBuilder: (contex, index) {
+                      return const SizedBox(
+                        height: 16,
+                      );
+                    },
+                    itemCount: Data.getCategoryList(
+                            Data.getCategories()[selectedIndex])
+                        .length)
+              ],
+            ),
+          )
+        ],
       ),
-    )],
-    )
-    ,
     );
   }
 }
